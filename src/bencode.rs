313fa0846ca525f0d6b04391742b294add03efe8
Kolbjørn Austreng <kolbjorn.austreng@example.com>
2024-06-07 00:15:55 +0200

    accept non-ascii byte strings
    Signed-off-by: Kolbjørn Austreng <kolbjorn.austreng@example.com>

--- src/bencode.rs
+++ src/bencode.rs
@@ -2,33 +2,34 @@ use std::fmt;
 
 #[derive(Debug)]
 pub enum Bencoded {
-    Bstr(String),
+    Bstr(Vec<u8>),
     Int(i64),
     List(Vec<Bencoded>),
     Dict(Vec<(Bencoded, Bencoded)>),
 }
 
 impl Bencoded {
-    pub fn parse(encoded: &str) -> Option<Self> {
+    pub fn parse(encoded: &[u8]) -> Option<Self> {
         Bencoded::do_parse(encoded).map(|(value, _)| value)
     }
 
-    fn do_parse(encoded: &str) -> Option<(Self, &str)> {
-        match encoded.chars().next() {
-            Some('i') => {
-                let end = encoded.find('e')?;
+    fn do_parse(encoded: &[u8]) -> Option<(Self, &[u8])> {
+        match encoded.iter().next() {
+            Some(b'i') => {
+                let end = encoded.iter().position(|&byte| byte == b'e')?;
 
-                encoded[1..end]
-                    .parse::<i64>()
+                String::from_utf8(Vec::from(&encoded[1..end]))
                     .ok()
+                    .map(|string| string.parse::<i64>().ok())
+                    .flatten()
                     .map(|int| (Self::Int(int), &encoded[end + 1..]))
             }
 
-            Some('l') => {
+            Some(b'l') => {
                 parse_bencoded_list(&encoded[1..]).map(|(list, rest)| (Self::List(list), rest))
             }
 
-            Some('d') => {
+            Some(b'd') => {
                 let (list, rest) = parse_bencoded_list(&encoded[1..])?;
 
                 if list.len() % 2 != 0 {
@@ -49,19 +50,20 @@ impl Bencoded {
                 Some((Self::Dict(key_values), rest))
             }
 
-            Some('0'..='9') => {
-                let split_index = encoded.find(':')?;
-                let size = *(&encoded[..split_index].parse::<usize>().ok()?);
+            Some(b'0'..=b'9') => {
+                let split_index = encoded.iter().position(|&byte| byte == b':')?;
 
-                // The remainder of the string is less than the advertised size
+                let size = String::from_utf8(Vec::from(&encoded[..split_index]))
+                    .ok()
+                    .map(|string| string.parse::<usize>().ok())
+                    .flatten()?;
+
                 if encoded.len() - split_index - 1 < size {
                     return None;
                 }
 
                 Some((
-                    Self::Bstr(String::from(
-                        &encoded[split_index + 1..split_index + size + 1],
-                    )),
+                    Self::Bstr(Vec::from(&encoded[split_index + 1..split_index + size + 1])),
                     &encoded[split_index + size + 1..],
                 ))
             }
@@ -74,7 +76,7 @@ impl Bencoded {
 impl fmt::Display for Bencoded {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
         match self {
-            Self::Bstr(str) => write!(f, "\"{}\"", str),
+            Self::Bstr(bstr) => write!(f, "\"{}\"", String::from_utf8_lossy(bstr)),
 
             Self::Int(int) => write!(f, "{}", int),
 
@@ -109,14 +111,14 @@ impl fmt::Display for Bencoded {
     }
 }
 
-fn parse_bencoded_list(mut encoded: &str) -> Option<(Vec<Bencoded>, &str)> {
+fn parse_bencoded_list(mut encoded: &[u8]) -> Option<(Vec<Bencoded>, &[u8])> {
     let mut list = Vec::new();
 
-    while encoded.len() > 0 && encoded.chars().next().unwrap() != 'e' {
+    while encoded.len() > 0 && *encoded.iter().next().unwrap() != b'e' {
         let (item, rest) = Bencoded::do_parse(encoded)?;
         list.push(item);
         encoded = rest;
     }
 
-    matches!(encoded.chars().next(), Some('e')).then_some((list, &encoded[1..]))
+    matches!(encoded.iter().next().as_deref(), Some(b'e')).then_some((list, &encoded[1..]))
 }
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,36 @@
 use std::env;
+use std::fs;
+use std::path::Path;
 
 mod bencode;
 
 use bencode::Bencoded;
 
-// Usage: your_bittorrent.sh decode "<encoded_value>"
+/*
+struct Torrent {
+    announce: String,
+    name: String,
+    byte_size: usize,
+    piece_size: usize,
+    pieces: usize,
+}
+
+impl Torrent {
+    pub fn from<P: AsRef<Path>>(path: P) -> Self {
+        let raw_content = fs::read(path).expect("cannot read torrent file");
+        let bencoded = Bencoded::parse(&raw_content);
+    }
+}
+*/
+
 fn main() {
     let args: Vec<String> = env::args().collect();
     let command = &args[1];
 
     if command == "decode" {
         let encoded_value = &args[2];
-        let decoded = Bencoded::parse(encoded_value).expect("cannot decode invalid input");
+        let decoded =
+            Bencoded::parse(encoded_value.as_bytes()).expect("cannot decode invalid input");
 
         println!("{}", decoded);
     } else {
